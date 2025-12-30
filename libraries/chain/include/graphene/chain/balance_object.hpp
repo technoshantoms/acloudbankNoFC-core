a235fd62fd8ed347f351b98d2ae9f6e0e7afeb81
Dennis K Satia <118351475+technoshantoms@users.noreply.github.com>
2025-12-30 11:50:53 +0300

    Update balance_object.hpp

--- libraries/chain/include/graphene/chain/balance_object.hpp
+++ libraries/chain/include/graphene/chain/balance_object.hpp
@@ -73,8 +73,6 @@ namespace graphene { namespace chain {
 
 MAP_OBJECT_ID_TO_TYPE(graphene::chain::balance_object)
 
-FC_REFLECT_TYPENAME( graphene::chain::balance_object )
-
 FC_REFLECT_DERIVED( graphene::chain::balance_object, (graphene::db::object),
                     (owner)(balance)(vesting_policy)(last_claim_date) )
 
